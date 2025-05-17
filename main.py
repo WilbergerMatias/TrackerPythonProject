d7728fc7f3efaf2811e634cea0f3e185577e566a
WilbergerMatias <matias180399@gmail.com>
2025-05-16 22:01:19 -0300

    se cambio la forma en que se muestra la flecha de velocidad para ir acorde al trackeo (1 de cada 3 frames capturado), estos cambios redujeron drasticamente el ruido percibido al realizar los analisis y los graficos, asi como tambien el impacto en los calculos realizados

--- main.py
+++ main.py
@@ -2,24 +2,22 @@ from config.video_utils import abrir_video, seleccionar_objeto, seleccionar_vide
 from tracker.escala import seleccionar_escala
 from tracker.tracker_automatico import trackear
 from cinematica.analisis import analizar_movimiento
-from cinematica.visualizar import dibujar_velocidad
 from graficos.utils import guardar_csv, guardarTXT
 from graficos.graficos import graficar_resultados
-
+from dinamica.fuerzas import calcular_fuerza_x_promedio_por_tramo
+from dinamica.visualizar import mostrar_tabla_fuerza_x_por_tramo
 
 def main():
     ruta = seleccionar_video()
     frame, video, fps = abrir_video(ruta)
     escala = seleccionar_escala(frame)
     bbox = seleccionar_objeto(frame)
-    
-    positions, times = trackear(video, bbox, escala, fps, 3)
+    positions, times = trackear(video, bbox, escala, fps)
     velocities, accelerations = analizar_movimiento(positions, times)
-    dibujar_velocidad(ruta, positions, times, escala)
     guardar_csv(times, positions, velocities, accelerations, "resultado.csv")
     guardarTXT(times, positions, velocities, accelerations, "resultados/datos_ultimo_movimiento.txt")
     graficar_resultados(times, positions, velocities, accelerations)
+    mostrar_tabla_fuerza_x_por_tramo(calcular_fuerza_x_promedio_por_tramo(times, accelerations[:, 0]))
     
-
 if __name__ == "__main__":
     main()
